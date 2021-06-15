cd3175027a35cb26e4d16f3ff16b037241b096cc
Priyanshu Gupta <guptapriyanshu973@gmail.com>
2021-06-15 14:22:28 +0530

    new lwarning

---
+++ main2.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname main2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(sqrt (+ (* 3 3) (* 4 4)))
+;Way to print a String in Racket
+"Hello, World!"
+;Adding the string is called string append which is called string-append function
+(string-append "Priyanshu" " " "Prasad" " " "Gupta")
